c272d186692b7a15c05528fb54c882adfaee97c3
Seelam-Mohith <seelammohith2222@gmail.com>
2026-06-30 02:39:00 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar() {
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ${
         scrolled
           ? 'glass shadow-lg shadow-black/10'
-          : 'bg-transparent'
+          : 'bg-bg-dark/80 border-b border-white/5'
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
